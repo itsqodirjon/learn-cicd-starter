bc4726a93a55b5cd8a0622a63ae6ca16f14a340e
itsQodirjon <mamatyoqubov0@gmail@email.com>
2026-06-14 15:20:43 +0500

    Fix empty file and add JSON response logic

---
+++ internal/auth/json.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	_, err = w.Write(dat)
+	if err != nil {
+		return
+	}
+}
